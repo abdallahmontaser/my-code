b8c18eb6b46414eeef477c70c0bd368ce5696690
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2021-09-06 16:43:05 +0200

    Update Graph DFS.cpp

--- Graph DFS.cpp
+++ Graph DFS.cpp
@@ -53,20 +53,18 @@ void dfs(int node,int par)
 
 // **************************************************************************************************
 //Checking a graph for acyclicity and finding a cycle in O(M)
-int is_cycle = 0, cycle_start, cycle_end;
+int cycle_start = -1, cycle_end;
 vector<vi>adj;
-vi color, parent;
-void dfs(int node)
+vi vis, parent;
+void dfs(int node, int par)
 {
-	color[node] = 1;
-	for (auto child : adj[node])
-	{
-		if (color[child] == 0)
-			parent[child] = node, dfs(child);
-		else if (color[child] == 1)
-			cycle_end = node, cycle_start = child, is_cycle = 1;
-	}
-	color[node] = 2;
+	vis[node] = 1;
+	for (int child : adj[node])
+		if (child != par)
+			if (vis[child])
+				cycle_end = child, cycle_start = node;
+			else
+				parent[child] = node, dfs(child, node);
 }
 void genrate_cycle_path()
 {
@@ -77,6 +75,23 @@ void genrate_cycle_path()
 		cycle.push_back(d), d = parent[d];
 	cycle.push_back(cycle_start);
 	reverse(cycle.begin(), cycle.end());
+	cout << cycle.size() << "\n";
 	for (int it : cycle)
-		cout << it+1 << " ";
+		cout << it + 1 << " ";
+}
+void solve()
+{
+	int N, M, u, v; cin >> N >> M;
+	adj = vector<vi>(N);
+	vis = vi(N, 0);
+	parent = vi(N, -1);
+	for (int i = 0; i < M; i++)
+		cin >> u >> v, u--, v--, adj[u].push_back(v), adj[v].push_back(u);
+	for (int i = 0; i < N && cycle_start == -1; i++)
+		if (!vis[i])
+			dfs(i, parent[i]);
+	if (cycle_start != -1)
+		genrate_cycle_path();
+	else
+		cout << "IMPOSSIBLE";
 }
