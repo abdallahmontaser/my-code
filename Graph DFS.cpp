b2a52696e019d9c9d0b1f2e27a2bf85646d81629
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2021-09-06 17:36:21 +0200

    Update Graph DFS.cpp

--- Graph DFS.cpp
+++ Graph DFS.cpp
@@ -14,48 +14,27 @@ void bipartite(int node, int color)
 }
 
 // **************************************************************************************************
-//directed graph
-int cycle = 0;
+//cycle directed graph
+int cycle_start = -1, cycle_end;
 vector<vi>adj;
-vi color;
+vi vis(1e5,0), parent(1e5,-1);
 void dfs(int node)
 {
 	color[node] = 1;
-	for (auto child : adj[node])
+	for (int child : adj[node])
 	{
-		if (color[child] == 0)
-			dfs(child);
+		if (color[child] == 0) 
+			parent[child] = node, dfs(child);
 		else if (color[child] == 1)
-			cycle = 1;
+			cycle_end = node, cycle_start = child;
 	}
 	color[node] = 2;
 }
 
 // **************************************************************************************************
-//undirected graph
-int cycle = 0;
-vector<vi>adj;
-vi  vis;
-void dfs(int node,int par)
-{
-	vis[node] = 1;
-	for (auto child : adj[node])
-	{
-		if (child != par)
-		{
-			if (vis[child] == 0)
-				dfs(child,node);
-			else
-				cycle = 1;
-		}
-	}
-}
-
-// **************************************************************************************************
-//Checking a graph for acyclicity and finding a cycle in O(M)
+//cycle undirected graph
 int cycle_start = -1, cycle_end;
-vector<vi>adj;
-vi vis, parent;
+vi vis(1e5,0), parent(1e5,-1);
 void dfs(int node, int par)
 {
 	vis[node] = 1;
@@ -66,6 +45,10 @@ void dfs(int node, int par)
 			else
 				parent[child] = node, dfs(child, node);
 }
+
+// **************************************************************************************************
+//Checking a graph for acyclicity and finding a cycle in O(M)
+int cycle_start = -1, cycle_end;
 void genrate_cycle_path()
 {
 	vi cycle;
@@ -78,20 +61,4 @@ void genrate_cycle_path()
 	cout << cycle.size() << "\n";
 	for (int it : cycle)
 		cout << it + 1 << " ";
-}
-void solve()
-{
-	int N, M, u, v; cin >> N >> M;
-	adj = vector<vi>(N);
-	vis = vi(N, 0);
-	parent = vi(N, -1);
-	for (int i = 0; i < M; i++)
-		cin >> u >> v, u--, v--, adj[u].push_back(v), adj[v].push_back(u);
-	for (int i = 0; i < N && cycle_start == -1; i++)
-		if (!vis[i])
-			dfs(i, parent[i]);
-	if (cycle_start != -1)
-		genrate_cycle_path();
-	else
-		cout << "IMPOSSIBLE";
 }
