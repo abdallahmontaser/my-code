b19565227c2c11e2dcb829030ec7c43736405b35
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2021-10-15 07:58:20 +0200

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -34,3 +34,50 @@ struct DSU
 		return X != Y;
 	}
 }DSU;
+
+
+const int MAXNODE = 100009;
+struct DSU
+{
+	int parent[MAXNODE];
+	int GroupSize[MAXNODE];
+	DSU()
+	{
+		for (int i = 0; i < MAXNODE; i++)
+		{
+			parent[i] = i;
+			GroupSize[i] = 1;
+		}
+	}
+	int FindLeader(int node)
+	{
+		if (parent[node] == node)
+			return node;
+		return parent[node] = FindLeader(parent[node]);
+	}
+	bool SameGroup(int u, int v)
+	{
+		return (FindLeader(u) == FindLeader(v));
+	}
+	void MergeGroups(int u, int v)
+	{
+		int leader1 = FindLeader(u);
+		int leader2 = FindLeader(v);
+		if (leader1 == leader2)
+			return;
+		if (GroupSize[leader1] > GroupSize[leader2])
+		{
+			parent[leader2] = leader1;
+			GroupSize[leader1] += GroupSize[leader2];
+		}
+		else
+		{
+			parent[leader1] = leader2;
+			GroupSize[leader2] += GroupSize[leader1];
+		}
+	}
+	int GetSize(int node)
+	{
+		return GroupSize[FindLeader(node)];
+	}
+};
