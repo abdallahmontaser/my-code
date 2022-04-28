e41fa85566a490af9e67037ef2f9a5bd9e22637c
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2022-04-28 06:50:05 +0200

    Update BIT.cpp

--- BIT.cpp
+++ BIT.cpp
@@ -17,3 +17,43 @@ void add(int idx, int val) {
 	}
 
 }
+
+
+struct fenwick_tree {
+	vector<int> BIT;
+	int n;
+	fenwick_tree(int n) :
+			n(n), BIT(n + 1) {
+	}
+	int getAccum(int idx) {
+		int sum = 0;
+		while (idx) {
+			sum += BIT[idx];
+			idx -= (idx & -idx);
+		}
+		return sum;
+	}
+	void add(int idx, int val) {
+		assert(idx != 0);
+		while (idx <= n) {
+			BIT[idx] += val;
+			idx += (idx & -idx);
+		}
+	}
+	int getValue(int idx) {
+		return getAccum(idx) - getAccum(idx - 1);
+	}
+	int getIdx(int accum) {
+		int start = 1, end = (int) BIT.size() - 1, rt = -1;
+		while (start <= end) {
+			int mid = start + end >> 1;
+			int val = getAccum(mid);
+			if (val >= accum)
+				rt = mid, end = mid - 1;
+			else
+				start = mid + 1;
+		}
+		return rt;
+	}
+};
+ 
