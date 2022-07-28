e31ed98288ac69c97e73b7dbf1dee8cd397003d5
abdallahmontaser <73659030+abdallahmontaser@users.noreply.github.com>
2022-07-28 08:20:40 +0200

    Create Euler Tour.cpp

---
+++ Euler Tour.cpp
@@ -0,0 +1,10 @@
+void euler(vector<vector<int>>& adjMax, vector<int>& ret, int n, int i, bool isDirected = false) {
+	for (int j = 0; j < n; j++) {
+		if (adjMax[i][j]) {
+			adjMax[i][j]--;
+			if (!isDirected)		adjMax[j][i]--;
+			euler(adjMax, ret, n, j, isDirected);
+		}
+	}
+	ret.push_back(i);
+}
